d8ebb4072fc4323c5da4f709ac5097850817048f
= <craigkrikorian@gmail.com>
2024-04-05 12:34:29 -0400

    Issue #16
    Co-authored-by: JonahOsborne <JonahOsborne@users.noreply.github.com>
    Co-authored-by: Aryan Sajith <asajith@umass.edu>

---
+++ src/client/scripts/generators/example_generator.js
@@ -0,0 +1,12 @@
+
+/**
+ * @todo write this documentation
+ * @param {Object} deck
+ * @returns {Element|null} the generated Deck Element (https://developer.mozilla.org/en-US/docs/Web/API/Element)
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Element}
+ */
+export default function generateDeck(deck) {
+
+
+
+}
