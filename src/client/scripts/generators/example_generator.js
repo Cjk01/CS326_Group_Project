0a7a64be57528c82c1cfefef3059999cecde8dac
= <craigkrikorian@gmail.com>
2024-04-05 12:53:51 -0400

    fix jsdocs

--- src/client/scripts/generators/example_generator.js
+++ src/client/scripts/generators/example_generator.js
@@ -3,10 +3,15 @@
  * @todo write this documentation
  * @param {Object} deck
  * @returns {Element|null} the generated Deck Element (https://developer.mozilla.org/en-US/docs/Web/API/Element)
- * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Element}
  */
 export default function generateDeck(deck) {
+    let deckElement = document.createElement("div");
+    deckElement.classList.add("deck");
+    deckElement.style.height = 100;
+    deckElement.style.width = 500;
 
+    let studyButton = document.createElement("input");
+    studyButton.onclick = "study();"
 
-
+    return deckElement;
 }
