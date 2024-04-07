ff33ec82f3bb0ff6598e76f0934b3c874239f399
daniilkoval1337 <daniilkoval2022@gmail.com>
2024-04-07 18:23:00 -0400

    Navbar generator

--- src/client/scripts/generators/navbar_generator.js
+++ src/client/scripts/generators/navbar_generator.js
@@ -1,1 +1,23 @@
+export function generateNavbar(navInputs, container) {
+    let nav = document.createElement('nav');
+    nav.setAttribute("id", "navbar");
+    let list = document.createElement('ul');
+    nav.appendChild(list);
+
+    for(let navInput in navInputs){
+
+        let listLink = document.createElement('a');
+        listLink.setAttribute("href", navInputs[navInput]);
+        listLink.innerText = navInput;
+
+        let listElem = document.createElement('li');
+        listElem.setAttribute("id", navInput + "OnNavBar");
+        listElem.classList.add("navbarLink");
+
+        listElem.appendChild(listLink);
+        list.appendChild(listElem);
+        list.appendChild(document.createElement("br"));
+    }
+    container.appendChild(nav);
+}
 
