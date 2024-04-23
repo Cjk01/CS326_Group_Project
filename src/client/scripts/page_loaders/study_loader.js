2919f269bfc1c4aa95c22699cbf5c95d6e50509a
Aryan Sajith <aryan.sajith04@gmail.com>
2024-04-23 12:28:51 -0400

    Fixed study page documentation and simplified conditional check

--- src/client/scripts/page_loaders/study_loader.js
+++ src/client/scripts/page_loaders/study_loader.js
@@ -3,16 +3,17 @@ import { Deck } from "../structures/deck.js";
 import { generateCard } from "../generators/card_generator.js";
 
 /**
- * This function is called once (in main.js).
- * It loads the study page if the user clicks on the page directly (as opposed to studying a deck specifically)
+ * This function is called in main.js.
+ * It loads the study page directly if the user clicks on it or with the deck if clicked on its entry
+ * @param {deck} deck - Optional deck specified if user clicks on its entry
  * @returns {Element} - The study view page
  * @todo Add general study page if 1 or more decks is actively being studied
 */
-export function loadGeneralStudyPageView(deck = null) {
+export function loadStudyPageView(deck = null) {
     const check_study_view = document.querySelector("div#StudyView");
     let final_study_view = null;
 
-    if (check_study_view === null || check_study_view.tagName !== "DIV") {
+    if (check_study_view === null) {
         final_study_view = document.createElement("div");
         final_study_view.setAttribute("id", "StudyView");
         final_study_view.className = "view";
@@ -35,26 +36,4 @@ export function loadGeneralStudyPageView(deck = null) {
     }
 
     return final_study_view;
-}
-
-/**
- * This function is called once (in entry.js).
- * It loads the study page if the user clicks on study from a deck entry (as opposed to clicking the general study page)
- * @param {Deck} deck - The specific deck whose entry was clicked on
- * @returns {Element} - The study view page
- * @todo Update study_loader.js to handle using the same view for the study(same StudyView ID) regardless of how you enter the Study Page
-*/
-export function loadDeckSpecificStudyPageView(deck) {
-    const user = User.getActiveUser();
-    const isDeckFreshlyRegistered = user.registerDeck(deck);
-
-
-
-    let study_view = document.createElement("div");
-
-    study_view.setAttribute("id", "StudyView");
-
-    study_view.appendChild(generateCard(deck.cards[0]));
-
-    return study_view;
 }
