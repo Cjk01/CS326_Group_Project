ac95cb1530a6a9a4925adf232747528ee8537919
= <craigkrikorian@gmail.com>
2024-05-07 12:29:57 -0400

    Cleanup + Add delete operations to express

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,11 @@
 import express from 'express';
-import cors from 'cors'; // I hope we dont need this later, but can be useful for testing
 import path from 'path';
 import {fileURLToPath} from 'url';
 import * as db from "./db.js";
 
-//we need these below as a cross platform solution
-//to acquire the current directory name while
-//using ES6 module syntax instead of the require syntax
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const client_path = path.resolve('src/client/');
 const app = express();
 const port = 3500;
 
-console.log(`client_path: ${client_path}`);
-console.log(`joined path: ${path.join(client_path, 'index.html')}`);
 
 //express middleware ---------------------------------------------
 //this sets up our app so that when users go to
@@ -41,7 +32,6 @@ app.post('/users' , async (req, res) => {
 });
 
 app.post('/decks', async (req, res) => {
-    console.log(req.body);
     let response_obj = await db.addDeckToDatabase(req.body);
     res.send(response_obj);
 });
@@ -56,7 +46,15 @@ app.put('/decks', async (req, res) => {
     res.send(response_obj);
 });
 
+app.delete('/users', async (req, res) => {
+    let response_obj = await db.deleteUserFromDatabase(req.query.id);
+    res.send(response_obj);
+});
 
+app.delete('/decks', async (req, res) => {
+    let response_obj = await db.deleteDeckFromDatabase(req.query.id);
+    res.send(response_obj);
+});
 
 app.delete('/clear_databases', async (req, res) => {
     let response_obj = await db.clearDatabases();
